6153ee709962f8cdcb968bf9da85f30227d8fcc8
Asmita Kolte <asmitaakolte@gmail.com>
2025-07-16 18:57:53 -0500

    indentation issue 2

--- test_app.py
+++ test_app.py
@@ -1,8 +1,7 @@
-import pytest
-
 from app import calculator
 
 def test_add(monkeypatch, capsys):
+
     inputs = iter(["2 + 3", "exit"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     
@@ -11,9 +10,11 @@ def test_add(monkeypatch, capsys):
     assert "= 5.0" in captured.out
 
 def test_invalid_input(monkeypatch, capsys):
+
     inputs = iter(["2++3", "exit"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     
     calculator()
     captured = capsys.readouterr()
-    assert "❌ Invalid input format" in captured.out
+    assert "Invalid input format" in captured.out
+
